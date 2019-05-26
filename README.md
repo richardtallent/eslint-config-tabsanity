435c38ed61b36aed07e9f79fe34ad895341b4ca9
richardtallent <richard@tallent.us>
2019-05-26 16:01:03 -0500

    first

---
+++ README.md
@@ -0,0 +1,19 @@
+
+This is a HIGHLY-OPINIONATED set of `package.json` defaults I use for my open-source and closed-source work.
+
+The following plugins are assumed:
+
+@vue/eslint-config-prettier
+eslint
+eslint-plugin-vue
+stylelint
+stylelint-config-standard
+
+The Vetur plugin is also assumed, with the HTML Formatter option set to NONE. I'd love to use js-beautfy-html, except for this issue:
+
+https://github.com/beautify-web/js-beautify/issues/1678
+
+This is SUBJECT TO CHANGE AT ANY TIME. I don't really expect anyone else to use this, as it's highly subjective to my personal taste
+in HTML, JS, and CSS formatting. I just did it because I got tired of having to copy and paste settings among all of my repos. But
+if you get value from it, great!
+
---
+++ index.js
@@ -0,0 +1,70 @@
+const ELEMENTS_CONTENT_WRAP_ALLOWED = ["pre", "textarea", "p", "b", "i", "a"]
+
+module.exports = {
+	extends: ["eslint:recommended", "plugin:vue/recommended", "prettier/vue"],
+	root: true,
+	env: {
+		node: true,
+	},
+	rules: {
+		"prettier/prettier": [
+			"warning",
+			{
+				useTabs: true,
+				semi: false,
+				singleQuote: false,
+				bracketSpacing: true,
+				printWidth: 160,
+				trailingComma: "es5",
+			},
+		],
+		indent: [
+			"error",
+			"tab",
+			{
+				SwitchCase: 1,
+			},
+		],
+		quotes: [
+			"error",
+			"double",
+			{
+				avoidEscape: true,
+			},
+		],
+		"no-console": "warn",
+		"comma-dangle": [
+			"error",
+			{
+				arrays: "ignore",
+				objects: "always-multiline",
+				imports: "ignore",
+				exports: "never",
+				functions: "ignore",
+			},
+		],
+		"vue/no-v-html": "off",
+		"vue/html-indent": ["warn", "tab"],
+		"vue/max-attributes-per-line": "off",
+		"vue/html-closing-bracket-newline": "never",
+		"vue/singleline-html-element-content-newline": [
+			"warning",
+			{
+				ignoreWhenNoAttributes: true,
+				ignoreWhenEmpty: true,
+				ignores: ELEMENTS_CONTENT_WRAP_ALLOWED,
+			},
+		],
+		"vue/multiline-html-element-content-newline": [
+			"warning",
+			{
+				ignoreWhenEmpty: true,
+				ignores: ELEMENTS_CONTENT_WRAP_ALLOWED,
+				allowEmptyLines: false,
+			},
+		],
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+}
